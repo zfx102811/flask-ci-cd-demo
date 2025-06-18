5018fb65069fc8b3e23e203b7f4b3ae9e7ba0daa
zfx <1028116783@qq.com>
2025-06-18 15:17:07 +0800

    1

--- app.py
+++ app.py
@@ -4,5 +4,10 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello, Docker!"
+def home():
+    return "Hello from Docker!"
+
+
+# 关键在这里：Flask 启动服务
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
